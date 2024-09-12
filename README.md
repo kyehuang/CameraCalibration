1c85f5f6dbda28c83083fff89e6b2aa75e2290f3
黃大師 <huangdashi@AUA.local>
2024-09-12 21:51:26 +0800

    [REFACTOR] refactor jpgtomp4

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ This project is to calibrate a camera using a set of chessboard images. The cali
 1. **Camera Calibration**: This step is to find the camera matrix and distortion coefficients. The camera matrix is used to project 3D points to 2D image points. The distortion coefficients are used to correct the distortion in the image.
 
 2. **Get Camera rotation matrix**: This step is to find the rotation matrix of the camera. The rotation matrix is used to rotate the camera to the world coordinate system.
+
+## Tools
+
+### Convert JPG to MP4
+To convert a series of JPG images into an MP4 video, follow these steps:
+
+1. Navigate to the `jpgtomp4` folder.
+2. Place the folder containing your JPG images inside the `jpgtomp4` directory.
+3. In the `jpgtomp4` folder, run the following command to generate the MP4 video:
+```
+# example
+python main.py --input_folder camera_1 --output_video chessboard_1.mp4 --frame_rate 30
+```
--- jpgtomp4/main.py
+++ jpgtomp4/main.py
@@ -1,21 +1,35 @@
 import cv2
 import os
+import argparse
 
 class jpgtomp4:
     def __init__(self, input_folder:str, output_video:str, frame_rate:int):
+        '''
+        input_folder: str
+            Folder containing jpg images
+        output_video: str
+            Output video file name
+        frame_rate: int
+            Frame rate of the output video
+
+        Example:
+        jpgtomp4("input_folder", "output_video", frame_rate).jpg_to_mp4()
+        '''
         self.input_folder = input_folder
         self.output_video = output_video
         self.frame_rate = frame_rate
     
     def jpg_to_mp4(self):
-        image_files = [os.path.join(self.input_folder, f) for f in os.listdir(self.input_folder) if f.endswith('.jpg')]
+        image_files = [os.path.join(self.input_folder, f)
+                        for f in os.listdir(self.input_folder) if f.endswith('.jpg')]
         image_files.sort()
 
         img = cv2.imread(image_files[0])
         height, width, _ = img.shape
 
         fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        video_writer = cv2.VideoWriter(self.output_video, fourcc, self.frame_rate, (width, height))
+        video_writer = cv2.VideoWriter(self.output_video, fourcc, self.frame_rate,
+                                       (width, height))
 
         for image_file in image_files:
             img = cv2.imread(image_file)
@@ -25,4 +39,18 @@ class jpgtomp4:
 
 if __name__ == "__main__":
     # jpgtomp4("input_folder", "output_video", frame_rate).jpg_to_mp4()
-    jpgtomp4("camera_1", "chessboard_1.mp4", 30).jpg_to_mp4()
+    # jpgtomp4("camera_1", "chessboard_1.mp4", 30).jpg_to_mp4()
+
+    # add argument parser
+    parser = argparse.ArgumentParser(description='Convert jpg images to mp4 video')
+
+    parser.add_argument('--input_folder', type=str, required=True,
+                        help='Folder containing jpg images')
+    parser.add_argument('--output_video', type=str, required=True,
+                        help='Output video file name')
+    parser.add_argument('--frame_rate', type=int, required=True,
+                        help='Frame rate of the output video')
+
+    args = parser.parse_args()
+
+    jpgtomp4(args.input_folder, args.output_video, args.frame_rate).jpg_to_mp4()
